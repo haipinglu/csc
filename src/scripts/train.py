c9fe683c151e11c5cacd958d268ea3b1e6b56955
lpjiang97 <prestonj@cs.washington.edu>
2020-05-11 15:01:18 -0700

    use session name argument

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -12,21 +12,21 @@ from src.scripts.plotting import plot_rf
 
 
 # save to tensorboard
-board = SummaryWriter("../../runs/sparse-net")
 arg = parse_args()
+board = SummaryWriter(f"../../runs/{arg.session_name}")
 # if use cuda
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # create net
-sparse_net = SparseNet(arg.n_neuron, arg.size, R_lr=arg.r_learning_rate, lmda=arg.reg, device=device)
+sparse_net = SparseNet(arg.n_neuron, arg.kernel_size, arg.stride_size, R_lr=arg.r_learning_rate, lmda=arg.reg, device=device)
 # load data
-dataloader = DataLoader(NatPatchDataset(arg.batch_size, arg.size, arg.size), batch_size=250)
+dataloader = DataLoader(NatPatchDataset(2000, arg.img_size, arg.img_size), batch_size=arg.batch_size)
 # train
 optim = torch.optim.SGD([{'params': sparse_net.U.weight, "lr": arg.learning_rate}])
-for e in range(arg.epoch):
+for e in tqdm(range(arg.epoch), desc="Epoch", total=arg.epoch):
     running_loss = 0
     c = 0
     for img_batch in tqdm(dataloader, desc='training', total=len(dataloader)):
-        img_batch = img_batch.reshape(img_batch.shape[0], -1).to(device)
+        img_batch = img_batch.reshape(img_batch.shape[0], 1, arg.img_size, arg.img_size).to(device)
         # update
         pred = sparse_net(img_batch)
         loss = ((img_batch - pred) ** 2).sum()
@@ -42,7 +42,7 @@ for e in range(arg.epoch):
     board.add_scalar('Loss', running_loss / c, e * len(dataloader) + c)
     if e % 5 == 4:
         # plotting
-        fig = plot_rf(sparse_net.U.weight.T.reshape(arg.n_neuron, arg.size, arg.size).cpu().data.numpy(), arg.n_neuron, arg.size)
+        fig = plot_rf(sparse_net.U.weight.reshape(arg.n_neuron, arg.kernel_size, arg.kernel_size).cpu().data.numpy(), arg.n_neuron, arg.kernel_size)
         board.add_figure('RF', fig, global_step=e * len(dataloader) + c)
     if e % 10 == 9:
         # save checkpoint
